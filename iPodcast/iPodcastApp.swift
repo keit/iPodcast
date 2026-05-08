23845c3bf5d173d0ec9275002cfc3b15c3be9adc
Kei Takashima <kei.takashima@crssoftware.co.nz>
2026-05-09 11:28:00 +1200

    Initial Commit

---
+++ iPodcast/iPodcastApp.swift
@@ -0,0 +1,17 @@
+//
+//  iPodcastApp.swift
+//  iPodcast
+//
+//  Created by Kei Takashima on 09/05/2026.
+//
+
+import SwiftUI
+
+@main
+struct iPodcastApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
